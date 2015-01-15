c0c47eb79d0671f70165e63139921125f744fee2
Zach Flanders <zachflanders@gmail.com>
2015-01-15 13:30:01 -0600

    asdf
    asfd

--- web/header.php
+++ web/header.php
@@ -9,7 +9,7 @@
     <link rel="icon" href="../../favicon.ico">
       
 
-<title>Starter Template for Bootstrap</title>
+<title>Flint Hills Dashboard</title>
 
     <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
