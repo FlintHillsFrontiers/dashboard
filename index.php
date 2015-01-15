378c11ae949926330db5b496bcf511af2b46023b
Zach Flanders <zachflanders@gmail.com>
2015-01-15 13:06:47 -0600

    test2
    test2

--- index.php
+++ index.php
@@ -9,8 +9,7 @@ $app['debug'] = true;
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   
-  echo "<!DOCTYPE html><html lang="en">  <head>    <meta charset="utf-8">    <meta http-equiv="X-UA-Compatible" content="IE=edge">    <meta name="viewport" content="width=device-width, initial-scale=1">    <meta name="description" content="">    <meta name="author" content="">    <link rel="icon" href="../../favicon.ico">";
-  echo "hello world"
+  return 'Hello, again.'
   
   
 });
