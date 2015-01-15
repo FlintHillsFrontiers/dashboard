ed44925455def068ad4753630677ef066a9ba32d
Zach Flanders <zachflanders@gmail.com>
2015-01-15 13:35:00 -0600

    remove hello world
    asdf

--- web/index.php
+++ web/index.php
@@ -14,7 +14,6 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
 // Our web handlers
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
-  return 'Hello World';
 });
 $app->run();
 ?>
