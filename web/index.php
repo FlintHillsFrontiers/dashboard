538d052c8e380136a1394863b7e3b55c62f327f1
Zach Flanders <zachflanders@gmail.com>
2015-01-15 12:40:55 -0600

    added heroku stuff
    added heroku stuff

--- web/index.php
+++ web/index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -8,6 +7,22 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="../../favicon.ico">
+      
+    <?php
+require('../vendor/autoload.php');
+$app = new Silex\Application();
+$app['debug'] = true;
+// Register the monolog logging service
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+  'monolog.logfile' => 'php://stderr',
+));
+// Our web handlers
+$app->get('/', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return 'Hello';
+});
+$app->run();
+?>
 
     <title>Starter Template for Bootstrap</title>
 
