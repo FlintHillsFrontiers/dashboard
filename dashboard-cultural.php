de3054ac195d15c6a88ee785f900e2afd0e4e9f2
Zach Flanders <zachflanders@gmail.com>
2015-04-21 14:12:30 -0500

    updated culture arts jobs chart

--- dashboard-cultural.php
+++ dashboard-cultural.php
@@ -29,6 +29,7 @@
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
         <script src="http://d3js.org/d3.v3.min.js" charset="utf-8"></script>
+	<script src="dashboard/tabletop.js"></script>
 	<script src="js/artjobs.js"></script>
 	<script>$(document).ready(function(){	
 			$("#cultural-menu").addClass('active');
